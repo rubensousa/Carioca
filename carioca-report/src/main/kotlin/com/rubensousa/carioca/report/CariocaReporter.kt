a39fae0a4918c4a0b494d4249f855e9198fbb2c8
Ruben Sousa <ruben@rubensousa.com>
2024-09-08 01:54:54 +0200

    Add docs to CariocaReporter

--- carioca-report/src/main/kotlin/com/rubensousa/carioca/report/CariocaReporter.kt
+++ carioca-report/src/main/kotlin/com/rubensousa/carioca/report/CariocaReporter.kt
@@ -7,10 +7,21 @@ import java.io.OutputStream
 
 interface CariocaReporter {
 
+    /**
+     * @param report the test report to be saved
+     * @param outputDir the root directory of the test storage
+     * @return The output directory for this report
+     */
     fun getOutputDir(report: TestReport, outputDir: Uri): String
 
+    /**
+     * @return the filename of the report
+     */
     fun getReportFilename(report: TestReport): String
 
+    /**
+     * @return the filename of the screenshot, excluding the extension
+     */
     fun getScreenshotName(id: String): String
 
     /**
