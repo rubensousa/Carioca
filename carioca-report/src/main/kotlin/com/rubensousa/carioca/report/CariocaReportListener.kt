e695bb5292c94971d479c7319e77c2edffa30870
Ruben Sousa <ruben@rubensousa.com>
2024-09-09 23:54:43 +0200

    Remove unnecessary permission

--- carioca-report/src/main/kotlin/com/rubensousa/carioca/report/CariocaReportListener.kt
+++ carioca-report/src/main/kotlin/com/rubensousa/carioca/report/CariocaReportListener.kt
@@ -1,12 +1,8 @@
 package com.rubensousa.carioca.report
 
 import android.annotation.SuppressLint
-import android.os.Build
 import androidx.test.internal.runner.listener.InstrumentationRunListener
-import androidx.test.platform.app.InstrumentationRegistry
-import androidx.test.uiautomator.UiDevice
 import com.rubensousa.carioca.report.internal.TestReportBuilder
-import com.rubensousa.carioca.report.internal.TestReportWriter
 import org.junit.runner.Description
 import org.junit.runner.Result
 
@@ -17,14 +13,6 @@ class CariocaReportListener : InstrumentationRunListener() {
 
     override fun testRunStarted(description: Description) {
         super.testRunStarted(description)
-        /**
-         * Ensures the target app can read the screen recordings collected
-         */
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val packageName = InstrumentationRegistry.getInstrumentation().targetContext.packageName
-            UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
-                .executeShellCommand("appops set --uid $packageName READ_MEDIA_VIDEO allow")
-        }
         reportBuilder.reset()
     }
 
