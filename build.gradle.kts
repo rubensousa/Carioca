3ebd6a60f0afb69fbe62d178204c3db23648f4fb
Ruben Sousa <ruben@rubensousa.com>
2024-09-22 20:29:50 +0200

    Publish API docs

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,15 +15,12 @@
  */
 
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
-subprojects {
-    apply(plugin = "org.jetbrains.dokka")
-}
 
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.jetbrains.kotlin.jvm) apply false
-    alias(libs.plugins.kotlin.dokka) apply false
+    alias(libs.plugins.kotlin.dokka) apply true
     alias(libs.plugins.maven.publish) apply false
 }
